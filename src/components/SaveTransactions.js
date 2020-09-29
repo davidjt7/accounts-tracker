f5d6d4d1b685620fcf8bf67fac8e92eaf88b08c2
David Joseph Thomas <djt.in7@gmail.com>
2020-09-29 22:25:49 +0530

    Added toast messages

--- src/components/SaveTransactions.js
+++ src/components/SaveTransactions.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 import { db } from '../firebase';
 import { getDate } from '../util';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const SaveTransactions = () => {
     const { transactions } = useContext(GlobalContext);
@@ -13,6 +15,7 @@ export const SaveTransactions = () => {
                 <button className="btn" onClick={saveCollection(transactions)}>
                     Save
                 </button>
+                <ToastContainer />
             </div>
         </>
     )
@@ -21,5 +24,10 @@ export const SaveTransactions = () => {
 const saveCollection = (transactions) => () => {
     db.collection("transactions")
         .doc(getDate())
-        .set({transactions});
+        .set({ transactions })
+        .then(notify("Transactions saved successfully!"))
+        .catch((e) => failNotify("Failed to save transactions: ", e.message));
 };
+
+const notify = (msg) => toast(msg, { autoClose: 2000 });
+const failNotify = (msg) => toast.error(msg, { autoClose: 2000 });
